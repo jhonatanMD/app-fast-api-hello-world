612242850831b316e3eea74a8a83bc2e8178d655
jhonatan <prov_zenta_jmallqui@resiter.cl>
2023-07-03 15:17:48 -0500

    request body y validaciones

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from pydantic import BaseModel
+
+from fastapi import FastAPI, Query
+from fastapi import Body
 
 app = FastAPI()
 
 
+# Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 @app.get("/get")
 async def get():
-    return {"message":"Hello World"}
+    return {"message": "Hello World"}
+
+#Body
+@app.post("/person/new")
+async def create_person(person: Person = Body(...)):
+    return person
+
+#QueryParam
+@app.get("/person/detail")
+async def show_person(
+        name: Optional[str] = Query(None, min_length=1, max_length=50 , title="Parametro de nombre de la persona"),
+        age: int = Query(... , title="Parametro de edad de la persona")
+):
+    return {name: age}
